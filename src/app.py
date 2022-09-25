530f4ffb1ba8cdf9cef20505ae105cc7d2bc277d
Tristan Bates <tristan.bates@gmail.com>
2022-09-24 22:38:53 -0400

    Fix import

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 import os
-import bloodbot.bloodbot
+from bloodbot.bloodbot import *
 
 
 def main(event, context):
     slack_webhook_url = os.getenv("SLACK_WEBHOOK_URL")
-    state_adapter = bloodbot.S3StateAdapter(os.getenv("STATE_BUCKET"), "state.json")
-    notification_adapter = bloodbot.SlackNotificationAdapter(
+    state_adapter = S3StateAdapter(os.getenv("STATE_BUCKET"), "state.json")
+    notification_adapter = SlackNotificationAdapter(
         webhook_url=slack_webhook_url
     )
 
-    app = bloodbot.App(
+    app = App(
         scraper=Scraper(hostname="www.bloodbrothersbrewing.com"),
         filters={"product_type": "beer"},
         fields=["title", "published_at", "images", "body_html", "handle"],
