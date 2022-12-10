51d88441590e6b56920518588a9043398269a9c1
Tristan Bates <tristan.bates@gmail.com>
2022-12-10 16:32:18 -0500

    sigh

--- main.tf
+++ main.tf
@@ -57,7 +57,6 @@ module "eventbridge" {
 
 module "lambda_function" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "~> 4.2"
 
   function_name = var.lambda_function_name
   description   = "Slack bot to notify when inventory changes"
@@ -65,12 +64,11 @@ module "lambda_function" {
   runtime       = "python3.8"
   publish       = true
 
-  source_path = [
-    {
-      path = "${path.module}/src",
-      pip_requirements = true,
-    }
-  ]
+  source_path = "${path.module}/src"
+
+  provisioner "local-exec" {
+    command = "pip install poetry"
+  }
 
   environment_variables = {
     SLACK_WEBHOOK_URL = var.slack_webhook_url
